3bd0c1fba8bf139a6514df1026e9d39b19972d45
HyunSeong <hhs950120@gmail.com>
2022-11-05 21:23:02 +0900

    깃허브 액션1

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("깃허브 액션");
+  res.send("깃허브 액션!!");
+});
+
+app.listen(3003, () => {
+  console.log("서버 열림");
+});
